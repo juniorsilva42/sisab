079dd5d3ca611bf47fa61b9255f58af254468022
jsiilva1 <jsiilva@outlook.com.br>
2018-11-22 03:17:47 -0200

    implementado o método Model para conexão ao banco de dados.

--- Core/Model.php
+++ Core/Model.php
@@ -7,5 +7,17 @@ use App\Config\Database;
 
 abstract class Model {
 
+    protected static function getConnection () {
 
+        static $db = null;
+
+        if ($db == null) {
+            $uri = 'mysql:host=' . Database::DB_HOST . ';dbname=' . Database::DB_NAME . ';charset=utf8';
+            $db = new PDO($uri, Database::DB_PASSWORD);
+
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+
+        return $db;
+    }
 }
