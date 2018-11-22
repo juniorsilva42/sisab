ea38eb8d05870f4dd29a9ee542f9b5900392770d
jsiilva1 <jsiilva@outlook.com.br>
2018-11-22 16:42:46 -0200

    Iniciando a implementação da Classe Router do pacote Core.

---
+++ Core/Router.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Core;
+
+class Router {
+
+    protected $routes = [];
+    protected $params = [];
+
+    public function add ($route, $params = []) { }
+
+    public function getRoutes () { }
+
+    public function getParams () {}
+}
--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
 <?php
 
-// Composer autoload
-require dirname(__DIR__).'/vendor/autoload.php';
-
 error_reporting(E_ALL);
+
+// Composer autoload
+require dirname(__DIR__) . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR . "autoload.php";
 
 use App\Sisab\ContaCorrente;
 use \App\Sisab\Agencia;
