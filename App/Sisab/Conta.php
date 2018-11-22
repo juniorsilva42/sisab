553375650f131f17b87e8c0c5944a2fae08ad18d
jsiilva1 <jsiilva@outlook.com.br>
2018-11-22 12:36:18 -0200

    Iniciando a implementação da classe Abstrata de Conta.

--- App/Sisab/Conta.php
+++ App/Sisab/Conta.php
@@ -2,4 +2,15 @@
 
 namespace App\Sisab;
 
-abstract class Conta {  }
+abstract class Conta {
+
+    protected $numero;
+    protected $saldo;
+
+    public function __construct($numeroConta) {
+        $this->numero = $numeroConta;
+        $this->saldo = 0;
+    }
+
+
+}
