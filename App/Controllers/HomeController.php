350ed047df0a2105f0a4ac38a1d4a0a76736fedb
jsiilva1 <jsiilva@outlook.com.br>
2018-11-23 16:58:08 -0200

    reestruturando a organiação lógica das views'

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -7,11 +7,12 @@ use Core\View;
 class HomeController extends \Core\Controller {
 
     public function indexAction () {
-        // View::renderTemplate('Home/index.html');
+        // View::renderTemplate('Home/skeleton.html');
 
-        View::renderTemplate('Home/index.html', [
+        View::renderTemplate('Home/skeleton.html', [
             'name' => 'Junior Silva',
-            'message' => 'i\'d\' like a coffee glass'
+            'message' => 'i\'d\' like a coffee glass',
+            'signature' => hash("sha256", "902ef2c77423503981468993d8aec16f.id")
         ]);
     }
 }
