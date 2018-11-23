7cad2bb85b10dd132d31aa9fc650e4268eb8f199
jsiilva1 <jsiilva@outlook.com.br>
2018-11-23 17:03:57 -0200

    alterada a estrutura das views

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -7,9 +7,7 @@ use Core\View;
 class HomeController extends \Core\Controller {
 
     public function indexAction () {
-        // View::renderTemplate('Home/skeleton.html');
-
-        View::renderTemplate('Home/skeleton.html', [
+        View::renderTemplate('Home/index.html', [
             'name' => 'Junior Silva',
             'message' => 'i\'d\' like a coffee glass',
             'signature' => hash("sha256", "902ef2c77423503981468993d8aec16f.id")
--- App/Controllers/OtherTestController.php
+++ App/Controllers/OtherTestController.php
@@ -2,13 +2,12 @@
 
 namespace App\Controllers;
 
-use Util\HttpHelpers;
+use Core\Util\HttpHelpers;
 
 class OtherTestController extends \Core\Controller {
 
     public function indexAction () {
         // echo ucwords(hash("sha256", "902ef2c77423503981468993d8aec16f.id"));
-
         echo HttpHelpers::getRestrictionId($_SERVER['QUERY_STRING']);
     }
 
