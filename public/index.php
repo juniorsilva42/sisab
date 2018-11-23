9ac8a62ae43b400b22b073afb472d7cbb88fb12b
jsiilva1 <jsiilva@outlook.com.br>
2018-11-23 21:26:18 -0200

    registrada as rotas

--- public/index.php
+++ public/index.php
@@ -10,6 +10,8 @@ require dirname(__DIR__) . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR 
 
 $router = new Core\Router();
 
+$router->iterateRoutes(\App\Http\IndexRoute::register());
 $router->iterateRoutes(\App\Http\ClientesRoute::register());
+$router->iterateRoutes(\App\Http\ContasRoute::register());
 
 $router->dispatch($_SERVER['QUERY_STRING']);
