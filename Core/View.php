fa12157edf3c5587aa13ecfe6015960149853152
jsiilva1 <jsiilva@outlook.com.br>
2018-11-23 15:54:19 -0200

    implementado o método render.

--- Core/View.php
+++ Core/View.php
@@ -1,2 +1,21 @@
 <?php
 
+namespace Core;
+
+class View {
+
+    public static function render ($view, $args = []) {
+        extract($args, EXTR_SKIP);
+        $file = dirname(__DIR__) . DIRECTORY_SEPARATOR . "App/Views/{$view}";
+
+        if (is_readable($file)):
+            require $file;
+        else:
+            throw new \Exception("A view {$file} não foi encontrada!");
+        endif;
+    }
+
+    public static function renderTemplate () {
+
+    }
+}
