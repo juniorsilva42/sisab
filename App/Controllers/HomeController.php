270e04f422b50d45c57eeb27b875ea7cdbc95b1e
jsiilva1 <jsiilva@outlook.com.br>
2018-11-23 16:14:17 -0200

    testando a template engine com passagem dinâmica de parâmetros.

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -7,6 +7,11 @@ use Core\View;
 class HomeController extends \Core\Controller {
 
     public function indexAction () {
-        View::renderTemplate('Home/index.html');
+        // View::renderTemplate('Home/index.html');
+
+        View::renderTemplate('Home/index.html', [
+            'name' => 'Junior Silva',
+            'message' => 'i\'d\' like a coffee glass'
+        ]);
     }
 }
