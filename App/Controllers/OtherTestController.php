299afca505fd30a24d01775a4389398e388abb5d
jsiilva1 <jsiilva@outlook.com.br>
2018-11-23 15:32:07 -0200

    adicionado um método recursivo para obter clausulas de ações nas rotas.

--- App/Controllers/OtherTestController.php
+++ App/Controllers/OtherTestController.php
@@ -5,6 +5,26 @@ namespace App\Controllers;
 class OtherTestController extends \Core\Controller {
 
     public function indexAction () {
-        echo ucwords(hash("sha256", "902ef2c77423503981468993d8aec16f.id"));
+        // echo ucwords(hash("sha256", "902ef2c77423503981468993d8aec16f.id"));
+
+        echo self::getAction($_SERVER['QUERY_STRING']);
+    }
+
+    public static function breakUrl ($url) {
+        if ($url != null) {
+            $uri = explode('/', $url);
+            return $uri;
+        }
+    }
+
+    public static function getAction ($url) {
+
+        $brokedUrl = self::breakUrl($url);
+        $url2 = [];
+
+        if ($brokedUrl != null)
+            return $brokedUrl[1];
+
+        return self::getAction($url);
     }
 }
