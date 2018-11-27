2fd2cc1fc35565b251befd5c99c5ba37c042432f
jsiilva1 <jsiilva@outlook.com.br>
2018-11-27 18:03:00 -0200

    refatorando...

--- App/Controllers/AgenciasController.php
+++ App/Controllers/AgenciasController.php
@@ -26,22 +26,28 @@ class AgenciasController extends \Core\Controller {
 
     public function criarAction () {
 
+        // Obtem os dados do formulário pela Query String do Request
         $nome = ($_REQUEST['nome']) ? $_REQUEST['nome'] : 'Agência Bradesco Sem nome';
         $numero = ($_REQUEST['numero']) ? $_REQUEST['numero'] : 'Agência Bradesco Sem nome';
         $endereco = ($_REQUEST['endereco']) ? $_REQUEST['endereco'] : 'Endereço Padrão';
         $capacidade = ($_REQUEST['capacidade']) ? $_REQUEST['capacidade'] : 500;
 
+        // Povoa o objeto pelos dados obtidos
         $agencia = new Agencia($numero, $nome, $endereco, $capacidade);
+
+        // Seta o estado pro model, onde vai utilizar esses dados para inserir no banco de dados
         $state = AgenciasModel::createNewAgency($agencia);
 
-        if ($state) {
+        // Controle as flash messages baseado no retorno do Model
+        if ($state):
             $flashMessage = 'A agência foi cadastrada com sucesso!';
             $alert = 'success';
-        } else {
+        else:
             $flashMessage = 'Erro ao inserir a agência!';
             $alert = 'danger';
-        }
+        endif;
 
+        // Renderiza o template implantando as variáveis de controle
         View::renderTemplate('Agencias/index', [
             'flashMessage' => $flashMessage,
             'flashAlert' => $alert,
