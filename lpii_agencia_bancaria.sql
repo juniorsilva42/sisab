57c3ca78e373a515e407a7e387555fc221809334
jsiilva1 <jsiilva@outlook.com.br>
2018-12-03 15:18:06 -0200

    adicionado o novo arquivo SQL contendo o novo schema do DB

--- lpii_agencia_bancaria.sql
+++ lpii_agencia_bancaria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 29-Nov-2018 às 04:55
+-- Generation Time: 03-Dez-2018 às 14:00
 -- Versão do servidor: 10.1.30-MariaDB
 -- PHP Version: 7.2.2
 
@@ -29,21 +29,22 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `agencias` (
-  `id_agencia` int(10) UNSIGNED NOT NULL,
-  `numero_agencia` varchar(15) NOT NULL,
-  `nome_agencia` varchar(50) NOT NULL,
-  `endereco` varchar(75) NOT NULL,
+  `id` int(10) UNSIGNED NOT NULL,
+  `numero` varchar(10) NOT NULL,
+  `nome` varchar(45) NOT NULL,
+  `endereco` varchar(75) DEFAULT NULL,
   `capacidade` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Extraindo dados da tabela `agencias`
 --
 
-INSERT INTO `agencias` (`id_agencia`, `numero_agencia`, `nome_agencia`, `endereco`, `capacidade`) VALUES
-(1, '7549-L', 'Agência Norte Bradesco', 'Rua dos Alfeneiros, Centro - 211', 1000000),
-(2, '0937-x', 'Agência Sudeste Bradesco', 'King\'s Landing, Porto Real, 110', 10000),
-(23, '3992-y', 'Agência Norte Banco do Brasil', 'BR 316 KM - 302, Zona Sul', 100000);
+INSERT INTO `agencias` (`id`, `numero`, `nome`, `endereco`, `capacidade`) VALUES
+(2, '0937-y', 'Agência Sudeste Bradesco', 'King\'s Landing, Porto Real, 220', 10),
+(23, '3992-y', 'Agência Norte Banco do Brasil', 'BR 316 KM - 302, Zona Sul', 100000),
+(25, 'LCM0001-x', 'La Casa da Moeda', 'Rua dos Alfeneiros, Hogwarts', 1000000),
+(26, 'Agência Br', 'Agência Bradesco Sem nome', 'Endereço Padrão', 500);
 
 -- --------------------------------------------------------
 
@@ -53,25 +54,20 @@ INSERT INTO `agencias` (`id_agencia`, `numero_agencia`, `nome_agencia`, `enderec
 
 CREATE TABLE `contas` (
   `id` int(10) UNSIGNED NOT NULL,
-  `numero` varchar(15) NOT NULL,
-  `saldo` int(11) NOT NULL,
+  `numero` varchar(15) CHARACTER SET latin1 NOT NULL,
+  `saldo` float DEFAULT NULL,
   `limite` int(11) DEFAULT NULL,
-  `rendimento` int(11) DEFAULT NULL,
-  `tipo` enum('CONTA_CORRENTE','CONTA_POUPANCA','CONTA_ESPECIAL') NOT NULL,
+  `rendimento` float DEFAULT NULL,
+  `tipo` enum('CONTA_CORRENTE','CONTA_POUPANCA','CONTA_ESPECIAL') CHARACTER SET latin1 NOT NULL,
   `fk_id_agencia` int(10) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Extraindo dados da tabela `contas`
 --
 
 INSERT INTO `contas` (`id`, `numero`, `saldo`, `limite`, `rendimento`, `tipo`, `fk_id_agencia`) VALUES
-(8, '1235', 660, NULL, 10, 'CONTA_POUPANCA', 1),
-(9, '1233x', 6750, 5000, NULL, 'CONTA_ESPECIAL', 2),
-(10, '1235-ax', 201, 5000, NULL, 'CONTA_ESPECIAL', 23),
-(11, '77382-z', 100, NULL, 5, 'CONTA_POUPANCA', 2),
-(12, '12378-yy', 1111, NULL, NULL, 'CONTA_CORRENTE', 1),
-(13, '123090001-J', 150, NULL, 3, 'CONTA_POUPANCA', 23);
+(36, '12AD0CP', 655, 0, 3, 'CONTA_POUPANCA', 2);
 
 --
 -- Indexes for dumped tables
@@ -81,13 +77,14 @@ INSERT INTO `contas` (`id`, `numero`, `saldo`, `limite`, `rendimento`, `tipo`, `
 -- Indexes for table `agencias`
 --
 ALTER TABLE `agencias`
-  ADD PRIMARY KEY (`id_agencia`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `numero_agencia` (`numero`);
 
 --
 -- Indexes for table `contas`
 --
 ALTER TABLE `contas`
-  ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`id`,`fk_id_agencia`,`tipo`),
   ADD UNIQUE KEY `numero` (`numero`),
   ADD KEY `fk_id_agencia` (`fk_id_agencia`);
 
@@ -99,13 +96,13 @@ ALTER TABLE `contas`
 -- AUTO_INCREMENT for table `agencias`
 --
 ALTER TABLE `agencias`
-  MODIFY `id_agencia` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `contas`
 --
 ALTER TABLE `contas`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- Constraints for dumped tables
@@ -115,8 +112,8 @@ ALTER TABLE `contas`
 -- Limitadores para a tabela `contas`
 --
 ALTER TABLE `contas`
-  ADD CONSTRAINT `contas_ibfk_1` FOREIGN KEY (`fk_id_agencia`) REFERENCES `agencias` (`id_agencia`),
-  ADD CONSTRAINT `fk_id_agencia` FOREIGN KEY (`fk_id_agencia`) REFERENCES `agencias` (`id_agencia`);
+  ADD CONSTRAINT `contas_ibfk_1` FOREIGN KEY (`fk_id_agencia`) REFERENCES `agencias` (`id`),
+  ADD CONSTRAINT `fk_id_agencia` FOREIGN KEY (`fk_id_agencia`) REFERENCES `agencias` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
